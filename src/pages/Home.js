379764b80e830548e621bca56d700d73d50ae30a
geekinthepink <rawisglenn@gmail.com>
2023-08-18 13:33:31 +0800

    Home Page: Add Search Bar and try-catch to axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState, useEffect, useRef } from "react";
 import AuthContext from "../context/authProvider";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
 import ItemCard from "../components/ItemCard";
 import axios from "axios";
 
@@ -9,13 +11,18 @@ const HomePage = () => {
   const [listProducts, setListProducts] = useState([]);
 
   async function getProduct() {
-    await axios.get("http://localhost:3001/products").then((res) => {
-      const products = res.data;
-      var arr = [];
-      Object.keys(products).forEach((key) => arr.push(products[key]));
-      setListProducts(arr);
-      // console.log("val.data", arr);
-    });
+    // Prevent Uncaught runtime errors: Network Err, if server not available
+    try {
+      await axios.get("http://localhost:3001/products").then((res) => {
+        const products = res.data;
+        var arr = [];
+        Object.keys(products).forEach((key) => arr.push(products[key]));
+        setListProducts(arr);
+        // console.log("val.data", arr);
+      });
+    } catch (error) {
+      console.log("Error", error);
+    }
   }
 
   useEffect(() => {
@@ -32,6 +39,47 @@ const HomePage = () => {
     <>
       <Container>
         <h1>Welcome {auth.user}!</h1>
+        <Row>
+          <Formik
+            onSubmit={async (values) => {
+              //await getAllStall();
+            }}
+          >
+            <Form>
+              <Container
+                style={{
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  width: "50%",
+                  marginTop: "20px",
+                  marginBottom: "20px",
+                }}
+              >
+                <Row
+                  style={{
+                    marginLeft: "0px",
+                    paddingLeft: "0px",
+                    marginTop: "8px",
+                  }}
+                >
+                  <Col md={12}>
+                    <Field
+                      id="searchStall"
+                      name="searchStall"
+                      placeholder="Search for stalls"
+                      onChange={(e) => {
+                        //setSearchInput(e.target.value);
+                        // console.log(searchInput);
+                        // searchFilter(e);
+                      }}
+                      style={{ width: "60%" }}
+                    />
+                  </Col>
+                </Row>
+              </Container>
+            </Form>
+          </Formik>
+        </Row>
         <Row
           style={{
             background: "#FFFFF0",
